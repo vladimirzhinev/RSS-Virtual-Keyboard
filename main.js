4116daa05a1cbc5eb883032122c86941b8c6ac0e
vladimirzhinev <vladimirzhinev@gmail.com>
2022-05-07 13:00:09 +0300

    feat: add functionality to keyboard

--- main.js
+++ main.js
@@ -75,37 +75,57 @@ const buts = [
 ];
 
 
-let lang = 'rus';
+let lng = 'rus';
 let capslock = false;
+const textArea = document.createElement('textarea');
+
+
+
+
+
+
+
+
+
+
 
 
 const bildKeyboard = () => {
    const wrapper = document.createElement('div');
-   const textArea = document.createElement('textarea');
-   const keyBoard = document.createElement('div');
    wrapper.className = 'wrapper';
    textArea.id = 'textArea';
+   const keyBoard = document.createElement('div');
    keyBoard.className = 'keyBoard';
+   const discription1 = document.createElement('div');
+   const discription2 = document.createElement('div');
+   discription1.className = 'discription';
+   discription1.textContent = `Клавиатура создана в операционной системе Windows`
+   discription2.className = 'discription';
+   discription2.textContent = `Для переключения языка ввода: ctrl + alt`
+
+
    wrapper.append(textArea);
    wrapper.append(keyBoard);
+   wrapper.append(discription1);
+   wrapper.append(discription2);
 
    for (let i = 0; i < buts.length; i++) {
       const row = document.createElement('div');
       row.classList.add('row');
-      for (let j = 0; j < buts[i].length; j += 1) {
+      for (let k = 0; k < buts[i].length; k++) {
          const key = document.createElement('div');
          key.classList.add('key');
-         key.classList.add(buts[i][j][0]);
+         key.classList.add(buts[i][k][0]);
 
          key.insertAdjacentHTML('afterBegin',
-            `<div class='rus'>
-              <span class='caseDown'>${buts[i][j][1]}</span>
-              <span class='caseUp hidden'>${buts[i][j][2]}</span>
-            </div>
-            <div class='eng hidden'>
-              <span class='caseDown hidden'>${buts[i][j][3]}</span>
-              <span class='caseUp hidden'>${buts[i][j][4]}</span>
-            </div>`);
+            `<div div class='rus' >
+              <span class='caseDown'>${buts[i][k][1]}</span>
+              <span class='caseUp hidden'>${buts[i][k][2]}</span>
+            </div >
+   <div class='eng hidden'>
+      <span class='caseDown hidden'>${buts[i][k][3]}</span>
+      <span class='caseUp hidden'>${buts[i][k][4]}</span>
+   </div>`);
 
          row.appendChild(key);
       }
@@ -113,4 +133,294 @@ const bildKeyboard = () => {
    }
    document.body.append(wrapper);
 }
-document.onload = bildKeyboard();
+
+document.onload = bildKeyboard(), textArea.focus();
+
+
+
+const unactive = (item) => {
+   item.classList.remove('active');
+}
+
+
+const active = (item) => {
+   item.classList.add('active');
+}
+
+
+
+
+const changeCase = () => {
+   keyBoard.querySelectorAll(`div > .${lng} `).forEach(it => {
+      it.querySelectorAll('span')[0].classList.toggle('hidden');
+      it.querySelectorAll('span')[1].classList.toggle('hidden');
+   })
+};
+
+
+
+const keyBoard = document.querySelector('.keyBoard')
+
+const changeLng = () => {
+
+   const prevLang = keyBoard.querySelectorAll(`div > .${lng} `);
+
+   prevLang.forEach(it => {
+      it.classList.toggle('hidden');
+      it.querySelectorAll('span')[0].classList.toggle('hidden');
+   })
+
+   if (lng === 'rus') {
+      lng = 'eng';
+      localStorage.setItem('lng', lng);
+   } else {
+      lng = 'rus';
+      localStorage.setItem('lng', lng);
+   }
+
+   const nextLang = keyBoard.querySelectorAll(`div > .${lng} `);
+   nextLang.forEach(it => {
+      it.classList.toggle('hidden');
+      it.querySelectorAll('span')[0].classList.toggle('hidden');
+   })
+
+};
+
+if (localStorage.lng === 'eng') {
+   changeLng();
+}
+
+
+
+
+
+
+window.addEventListener('keydown', (e) => {
+
+   const item = keyBoard.getElementsByClassName(e.code)[0];
+   if (e.altKey && e.ctrlKey) {
+      active(item);
+      changeLng();
+      return false;
+   }
+
+   let pos = textArea.selectionStart;
+   let posEnd = textArea.selectionEnd;
+
+   switch (e.code) {
+
+      case 'MetaLeft':
+         active(item);
+         break;
+
+      case 'Tab':
+         e.preventDefault();
+         active(item);
+         textArea.value += '  ';
+         break;
+
+      case 'Enter':
+         active(item);
+         break;
+
+      case 'CapsLock':
+
+         (capslock) ? (unactive(item), capslock = false) : (active(item), capslock = true)
+         changeCase();
+         break;
+
+
+      case 'Backspace':
+         e.preventDefault();
+         if (pos < 1) break
+         textArea.value = textArea.value.slice(0, pos - 1) + textArea.value.slice(pos);
+         pos--;
+         posEnd--;
+         textArea.selectionStart = pos;
+         textArea.selectionEnd = posEnd;
+         active(item);
+         break;
+
+      case 'Delete':
+         e.preventDefault();
+         textArea.value = textArea.value.slice(0, pos) + textArea.value.slice(pos + 1);
+         textArea.selectionStart = pos;
+         textArea.selectionEnd = posEnd;
+         active(item);
+         break;
+      case 'AltLeft':
+      case 'AltRight':
+         e.preventDefault();
+         active(item);
+         break;
+      case 'ControlLeft':
+      case 'ControlRight':
+
+         active(item);
+         break;
+      case 'ShiftLeft':
+      case 'ShiftRight':
+         active(item);
+         changeCase();
+         break;
+
+      default:
+         e.preventDefault();
+         textArea.value = textArea.value.slice(0, pos) + item.querySelectorAll(':not(.hidden)')[1].textContent + textArea.value.slice(pos);
+         pos++;
+         textArea.selectionStart = pos;
+         textArea.selectionEnd = pos;
+         active(item);
+         break;
+   }
+});
+
+
+
+
+keyBoard.addEventListener('mousedown', (e) => {
+
+   let key = e.target.closest('.key')
+
+   if (!key) return;
+   const item = key
+   let pos = textArea.selectionStart;
+   let posEnd = textArea.selectionEnd;
+   e.preventDefault();
+
+   switch (item.classList[1]) {
+
+      case 'Tab':
+         textArea.value += '  ';
+         active(item);
+         break;
+
+      case 'Enter':
+         textArea.value = textArea.value.slice(0, pos) + '\n' + textArea.value.slice(pos);
+         pos++;
+         textArea.selectionStart = pos;
+         textArea.selectionEnd = pos;
+         active(item);
+         break;
+
+      case 'Delete':
+         active(item);
+         textArea.value = textArea.value.slice(0, pos) + textArea.value.slice(pos + 1);
+         textArea.selectionStart = pos;
+         textArea.selectionEnd = pos;
+         break;
+
+      case 'Backspace':
+         active(item);
+         if (pos < 1) break
+         textArea.value = textArea.value.slice(0, pos - 1) + textArea.value.slice(pos);
+         pos--;
+         posEnd--;
+         textArea.selectionStart = pos;
+         textArea.selectionEnd = pos;
+         break;
+
+      case 'CapsLock':
+         active(item);
+         if (capslock) {
+            unactive(item);
+            capslock = false;
+         } else {
+            active(item);
+            capslock = true;
+         }
+         changeCase();
+         break;
+      case 'ShiftLeft':
+      case 'ShiftRight':
+
+         active(item);
+         changeCase();
+
+         break;
+
+      case 'ControlLeft':
+      case 'ControlRight':
+
+         active(item);
+         break;
+
+      default:
+
+         textArea.value = textArea.value.slice(0, pos) + item.querySelectorAll(':not(.hidden)')[1].textContent + textArea.value.slice(pos);
+         pos++;
+         textArea.selectionStart = pos;
+         textArea.selectionEnd = pos;
+         active(item);
+         break;
+
+      case 'AltLeft':
+      case 'AltRight':
+
+         active(item);
+         break;
+
+
+      case 'MetaLeft':
+         active(item);
+         break;
+   }
+
+});
+
+
+
+
+document.addEventListener('mouseup', (e) => {
+
+   let key = e.target.closest('.key')
+
+   if (!key) return
+   if (key) {
+      const item = key;
+
+
+      switch (item.classList[1]) {
+         case 'ShiftLeft':
+         case 'ShiftRight':
+            e.preventDefault();
+            unactive(item);
+            changeCase();
+            break;
+         case 'CapsLock':
+            if (capslock !== true) {
+               unactive(e.target.closest('.key'));
+            } else {
+               active(e.target.closest('.key'));
+            }
+            break;
+
+         case 'ControlLeft':
+         case 'ControlRight':
+            unactive(item);
+            break;
+         default:
+            unactive(item);
+            break;
+      }
+   }
+});
+
+document.addEventListener('keyup', (e) => {
+   if (e.key !== 'CapsLock') {
+      if (e.key === 'Shift') {
+         changeCase();
+      }
+
+      const item = keyBoard.getElementsByClassName(e.code)[0];
+
+
+      unactive(item);
+
+
+   }
+
+
+
+
+});
